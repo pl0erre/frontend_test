562d35bf714da78ab20d81281b081bb204a47288
pl0erre <luca.angona@web.de>
2019-12-28 15:05:22 +0100

    Moved data to separate file; dynamic content loading; started working on responsive design

---
+++ assets/js/data.js
@@ -0,0 +1,92 @@
+// Data for Newsfeed
+const newsData = 
+    [{
+        img: "https://cdn.pixabay.com/photo/2017/12/01/16/14/cinnamon-stars-2991174_960_720.jpg",
+        date: "21.12.2019",
+        category: "Feiertage",
+        subheading: "",
+        heading: "Frohe Weihnachten und ein gesundes Jahr 2020",
+        text: 
+        `
+        Die Uniklinik Köln wünscht Ihnen besinnliche, frohe Weihnachtsfeiertage im Kreise Ihrer Liebsten und einen guten Rutsch in ein gesundes, erfolgreiches Jahr 2020!
+        `
+    }, {
+        img: "https://cdn.pixabay.com/photo/2015/08/25/03/50/aroma-906137_960_720.jpg",
+        date: "23.12.2019",
+        category: "Frauenheilkunde",
+        subheading: "Gruppe Köln - Uniklinik der Frauenselbsthilfe nach Krebs NRW e.V.",
+        heading: "mutig-bunt-aktiv: Neue Selbsthilfegruppe gegründet",
+        text: 
+        `
+        In der Klinik und Poliklinik für Frauenheilkunde und Geburtshilfe der Uniklinik Köln ist die Gruppe Köln – Uniklinik der Frauenselbsthilfe nach Krebs NRW e.V. neu gegründet worden. Eine Gruppe aus etwa zwanzig betroffenen Frauen wählte in der feierlichen Gründungsveranstaltung am 10. Dezember 2019 Monika Klöver-Trompetter zur Gruppenleiterin, Eva Müllenborn zu ihrer Stellvertreterin und Maria Herbei zur Kassiererin. Univ.-Prof. Dr. Peter Mallmann, Direktor der Klinik und Poliklinik für Frauenheilkunde und Geburtshilfe der Uniklinik Köln, gratulierte und begrüßte die Neugründung: „Eine Krebsdiagnose schüttelt das ganze Leben durcheinander, alle Bereiche werden in Frage gestellt. Während, aber eben auch gerade nach dem Abschluss einer Therapie, ist der Weg in ein ,normales’ Leben schwierig und nicht immer allein zu meistern. Der Austausch mit anderen Betroffenen, die dabei vielleicht schon ein paar Schritte weiter sind, kann dann eine wesentliche Stütze sein. Ich freue mich, dass an Krebs erkrankte Frauen in der Uniklinik Köln und der ganzen Stadt mit der heutigen Gründung dieser Gruppe wieder ein Selbsthilfeangebot zur Verfügung steht.“
+        `
+    }, {
+        img: "https://cdn.pixabay.com/photo/2016/02/01/00/56/news-1172463_960_720.jpg",
+        date: "20.12.2019",
+        category: "Presseeinladung",
+        subheading: "Pressegespräch – DAK-Gesundheitsreport 2019",
+        heading: "»Trinken, Dampfen, Gamen«",
+        text: 
+        `
+        Kurz vor Jahreswechsel präsentieren die Krankenkasse DAK-Gesundheit und die Uniklinik Köln gemeinsam in einem Pressegespräch den aktuellen Gesundheitsreport 2019. Die Krankenkasse untersucht in ihrer aktuellen Studie mit dem Schwerpunkt „Trinken, Dampfen, Gamen“ wie viele Erwerbstätige in Nordrhein-Westfalen mit gravierenden Problemen durch Alkohol, Zigaretten und Computerspiele zu kämpfen haben. Das Gespräch thematisiert dabei auch Ursachen und Risikofaktoren bei Kindern- und Jugendlichen. Im Jahr 2018 ist der Krankenstand in Köln gestiegen. Welche Rolle spielen dabei Suchtprobleme? Wie haben sie sich in den vergangenen Jahren entwickelt? Welche Sucht verursacht die größten sozialen und gesundheitlichen Schäden? Wie hoch ist der Anteil an riskanten Gamern? Welche weiteren Entwicklungen gab es? Welche Besonderheiten sind bei einzelnen Diagnosen zu beobachten?
+        `
+    }]
+
+// Data for Eventfeed
+const eventsData = 
+    [{
+        img: "",
+        date: "09.01.2020",
+        time: "16:00 Uhr",
+        category: "Fachgebiet: Kardiologie",
+        subheading: "",
+        heading: "Non-coding RNAs in aortic aneurysm disease",
+        text: 
+        `
+        We are very pleased to invite you to the seminar of the SFB TRR259 - Aortic Disease - held by Prof. Lars Mägdefessel on 'Non-coding RNAs in aortic aneurysm disease'.
+
+        Venue:
+        Seminar room 6/7
+        Teaching Building No. 10
+        Universitätsklinikum Bonn
+        Venusberg-Campus 1
+        53127 Bonn
+        
+        We will organize a livestream of the seminar to Cologne:
+        Mediathek
+        Center for Molecular Medicine Cologne (CMMC)
+        University Hospital of Cologne
+        Robert-Koch-Straße 21
+        50931 Cologne
+        `
+    }, {
+        img: "",
+        date: "10.01.2020 - 11.01.2020",
+        time: "",
+        category: "Fachgebiet: Radiologie , Pathologie , Kardiologie",
+        subheading: "",
+        heading: "Update RSNA - Thorax",
+        text: 
+        `
+        Das Institut und Poliklinik für Radiologische Diagnostik und Interventionelle Radiologie der Unikliniik Köln lädt herzlich zur nächsten Fortbildung in Radiologischer Diagnostik in das Maternushaus Köln ein.
+
+        Thema: Update RSNA - Thorax
+
+        Programm:
+        Freitag, 10. Januar 2020, 09:00 - 17:45 Uhr
+        Samstag, 11. Januar 2020, 09:00 - 16:20 Uhr
+
+        Für diese Veranstaltung wurde die CME-Zertifizierung bei der Ärztekammer Nordrhein beantragt.
+        `
+    }, {
+        img: "",
+        date: "10.01.2020",
+        time: "07:15 - 08:00 Uhr",
+        category: "Fachgebiet: Orthopädie und Unfallchirurgie",
+        subheading: "",
+        heading: "Journalclub",
+        text: 
+        `
+        Die Klinik und Poliklinik für Orthopädie und Unfallchirurgie der Uniklinik Köln lädt herzlich zum Journalclub (Stavros Oikonomidis, Peter Knöll) im Rahmen der Freitagsfortbildung ein.
+        `
+    }]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,120 @@
-
-// 
-// Data for Newsfeed
-const newsData = 
-    [{
-        date: "21.12.2019",
-        category: "Feiertage",
-        subheading: "",
-        heading: "Frohe Weihnachten und ein gesundes Jahr 2020",
-        text: 
-        `
-        Die Uniklinik Köln wünscht Ihnen besinnliche, frohe Weihnachtsfeiertage im Kreise Ihrer Liebsten und einen guten Rutsch in ein gesundes, erfolgreiches Jahr 2020!
-        `
-    }, {
-        date: "23.12.2019",
-        category: "Frauenheilkunde",
-        subheading: "Gruppe Köln - Uniklinik der Frauenselbsthilfe nach Krebs NRW e.V.",
-        heading: "mutig-bunt-aktiv: Neue Selbsthilfegruppe gegründet",
-        text: 
-        `
-        In der Klinik und Poliklinik für Frauenheilkunde und Geburtshilfe der Uniklinik Köln ist die Gruppe Köln – Uniklinik der Frauenselbsthilfe nach Krebs NRW e.V. neu gegründet worden. Eine Gruppe aus etwa zwanzig betroffenen Frauen wählte in der feierlichen Gründungsveranstaltung am 10. Dezember 2019 Monika Klöver-Trompetter zur Gruppenleiterin, Eva Müllenborn zu ihrer Stellvertreterin und Maria Herbei zur Kassiererin. Univ.-Prof. Dr. Peter Mallmann, Direktor der Klinik und Poliklinik für Frauenheilkunde und Geburtshilfe der Uniklinik Köln, gratulierte und begrüßte die Neugründung: „Eine Krebsdiagnose schüttelt das ganze Leben durcheinander, alle Bereiche werden in Frage gestellt. Während, aber eben auch gerade nach dem Abschluss einer Therapie, ist der Weg in ein ,normales’ Leben schwierig und nicht immer allein zu meistern. Der Austausch mit anderen Betroffenen, die dabei vielleicht schon ein paar Schritte weiter sind, kann dann eine wesentliche Stütze sein. Ich freue mich, dass an Krebs erkrankte Frauen in der Uniklinik Köln und der ganzen Stadt mit der heutigen Gründung dieser Gruppe wieder ein Selbsthilfeangebot zur Verfügung steht.“
-        `
-    }, {
-        date: "20.12.2019",
-        category: "Presseeinladung",
-        subheading: "Pressegespräch – DAK-Gesundheitsreport 2019",
-        heading: "»Trinken, Dampfen, Gamen«",
-        text: 
-        `
-        Kurz vor Jahreswechsel präsentieren die Krankenkasse DAK-Gesundheit und die Uniklinik Köln gemeinsam in einem Pressegespräch den aktuellen Gesundheitsreport 2019. Die Krankenkasse untersucht in ihrer aktuellen Studie mit dem Schwerpunkt „Trinken, Dampfen, Gamen“ wie viele Erwerbstätige in Nordrhein-Westfalen mit gravierenden Problemen durch Alkohol, Zigaretten und Computerspiele zu kämpfen haben. Das Gespräch thematisiert dabei auch Ursachen und Risikofaktoren bei Kindern- und Jugendlichen. Im Jahr 2018 ist der Krankenstand in Köln gestiegen. Welche Rolle spielen dabei Suchtprobleme? Wie haben sie sich in den vergangenen Jahren entwickelt? Welche Sucht verursacht die größten sozialen und gesundheitlichen Schäden? Wie hoch ist der Anteil an riskanten Gamern? Welche weiteren Entwicklungen gab es? Welche Besonderheiten sind bei einzelnen Diagnosen zu beobachten?
-        `
-    }]
-
-// Data for Eventsfeed
-const eventsData = 
-    [{
-        date: "09.01.2020",
-        time: "16:00 Uhr",
-        category: "Fachgebiet: Kardiologie",
-        subheading: "",
-        heading: "Non-coding RNAs in aortic aneurysm disease",
-        text: 
-        `
-        We are very pleased to invite you to the seminar of the SFB TRR259 - Aortic Disease - held by Prof. Lars Mägdefessel on 'Non-coding RNAs in aortic aneurysm disease'.
-
-        Venue:
-        Seminar room 6/7
-        Teaching Building No. 10
-        Universitätsklinikum Bonn
-        Venusberg-Campus 1
-        53127 Bonn
-        
-        We will organize a livestream of the seminar to Cologne:
-        Mediathek
-        Center for Molecular Medicine Cologne (CMMC)
-        University Hospital of Cologne
-        Robert-Koch-Straße 21
-        50931 Cologne
-        `
-    }, {
-        date: "10.01.2020 - 11.01.2020",
-        time: "",
-        category: "Fachgebiet: Radiologie , Pathologie , Kardiologie",
-        subheading: "",
-        heading: "Update RSNA - Thorax",
-        text: 
-        `
-        Das Institut und Poliklinik für Radiologische Diagnostik und Interventionelle Radiologie der Unikliniik Köln lädt herzlich zur nächsten Fortbildung in Radiologischer Diagnostik in das Maternushaus Köln ein.
-
-        Thema: Update RSNA - Thorax
-
-        Programm:
-        Freitag, 10. Januar 2020, 09:00 - 17:45 Uhr
-        Samstag, 11. Januar 2020, 09:00 - 16:20 Uhr
-
-        Für diese Veranstaltung wurde die CME-Zertifizierung bei der Ärztekammer Nordrhein beantragt.
-        `
-    }, {
-        date: "10.01.2020",
-        time: "07:15 - 08:00 Uhr",
-        category: "Fachgebiet: Orthopädie und Unfallchirurgie",
-        subheading: "",
-        heading: "Journalclub",
-        text: 
-        `
-        Die Klinik und Poliklinik für Orthopädie und Unfallchirurgie der Uniklinik Köln lädt herzlich zum Journalclub (Stavros Oikonomidis, Peter Knöll) im Rahmen der Freitagsfortbildung ein.
-        `
-    }]
-
-
-
 document.addEventListener("DOMContentLoaded", () => { 
 
-    // Constants
-    const tabs = document.getElementsByClassName("tab")
-    const randTabNr = Math.floor(Math.random() * 2);
-    
-    // Select a random Tab after Pageload
-    tabs[randTabNr].className= "tab active";
+    // -- Constants -- //
+    const numberOfInfoPreviews = 3;
+    const numberOfTabs = 2;
+    const seeMoreMsg = "Mehr Informationen"
+    const backupImgUrl = "https://cdn1.iconfinder.com/data/icons/hawcons/32/699055-icon-65-document-image-512.png"
+    const tabs = document.getElementsByClassName("tab");
+    const news = tabs[0];
+    const events = tabs[1];
+    const infoParCont = document.getElementsByClassName("infobar__content")[0];
 
-    //Functions
+    // -- Functions -- //
+    // Select a random Tab after Pageload
+    function selectRandomTab() {
+        tabs[Math.floor(Math.random() * numberOfTabs)].classList.add("active");
+        loadActiveContent();
+    }
+
+    // write content of selected feed
+    function writeActiveContent(infoChild) {
+        let infoBox = document.createElement("div");
+        let imgElem = document.createElement("img");
+        let infoItem = document.createElement("div");
+        let dateElem = document.createElement("p");
+        let headingElem = document.createElement("h3");
+        let subheadingElem = document.createElement("h4");
+        let seeMoreElem = document.createElement("a");
+        // add classes
+        infoItem.classList.add("infobar__item");
+        imgElem.classList.add("infobar__image");
+        infoBox.classList.add("infobar__details")
+        // Check if image url is given; if not: use default src
+        if (infoChild.img) {
+            imgElem.src = infoChild.img;
+        } else {
+            imgElem.src = backupImgUrl;
+        }
+        // add link Url
+        seeMoreElem.href = "#"
+        // alter inner html
+        dateElem.innerHTML = infoChild.date;
+        headingElem.innerHTML = infoChild.heading;
+        subheadingElem.innerHTML = infoChild.subheading;
+        seeMoreElem.innerHTML = seeMoreMsg;
+        // construct InfoItem
+        infoItem.appendChild(imgElem);
+        infoItem.appendChild(infoBox);
+        infoBox.appendChild(dateElem);
+        infoBox.appendChild(headingElem);
+        infoBox.appendChild(subheadingElem);
+        infoBox.appendChild(seeMoreElem);
+        // append infoItem
+        infoParCont.appendChild(infoItem);
+    }
+
+    // clear all content of selected feed; remove all childelements
+    function clearOldContent() {
+        while (infoParCont.firstChild) {
+            infoParCont.removeChild(infoParCont.firstChild);
+        }
+    }
+
+    // load content for selected feed
+    function loadActiveContent() {
+        // load news content if news tab is selected
+        if (news.classList.contains("active")) {
+            for (let i = 0; i < numberOfInfoPreviews; i++) {
+                let  j = newsData[i];
+                let infoChild = {
+                    img: j.img,
+                    date: j.date,
+                    subheading: j.subheading,
+                    heading: j.heading
+                }
+                // pass data to writefunction 
+                writeActiveContent(infoChild);  
+            }
+        // load events if event tab is selected
+        } else if (events.classList.contains("active")) {
+            for (let i = 0; i < numberOfInfoPreviews; i++) {
+                let  j = eventsData[i];
+                let infoChild = {
+                    date: j.date,
+                    subheading: j.subheading,
+                    heading: j.heading
+                }
+                // pass data to writefunction 
+                writeActiveContent(infoChild);  
+            }
+        }
+    }
+
+    // clear all active tabs of infobar
     function clearActiveTabs() {
         for ( let j = 0; j < tabs.length; j++) {
             tabs[j].classList.remove("active");
         }
     }
 
-    // Add eventlisteners to Tabs and add or remove "active" class on click
+    // Add eventlisteners to Infobar tabs and add or remove "active" class on click
     for (let i = 0; i < tabs.length; i++) {
 
         tabs[i].addEventListener("click", (e) => {
             let elem = e.target;
             let classList = elem.classList
+            // Clear all active tabs
+            clearActiveTabs();
+            // Add active class to selected tab
+            classList.add("active");
+            // remove all entried from before
+            clearOldContent();
+            // load content
+            loadActiveContent();
 
-            if (!classList.contains("active")) {
-                // Clear all active tabs
-                clearActiveTabs();
-                // Add active class to selected tab
-                classList.add("active");
-            } 
         })
     }
+    // load random tab on pageload
+    selectRandomTab();
 });
